9dc613920d4b86c6c2c565ff18cbc5d823bca3f6
lavakumar578 <lavakumar578@gmail.com>
2022-08-12 12:46:23 +0530

    commit

--- src/main/java/com/mykidsbank/ObjectRepository/DeleteDepositAccountsPage.java
+++ src/main/java/com/mykidsbank/ObjectRepository/DeleteDepositAccountsPage.java
@@ -7,8 +7,9 @@ import org.openqa.selenium.support.PageFactory;
 
 public class DeleteDepositAccountsPage {
 
-	@FindBy(xpath="//span[@id=\"check_uncheck_boxes_id\"]")
+	@FindBy(xpath="//span[contains(text(),\"select\")]")
 	private WebElement selectAllBtn;
+	
 	@FindBy(xpath="//span[text()=\"Submit\"]")
 	private WebElement submitBtn;
 	public DeleteDepositAccountsPage(WebDriver driver) {
