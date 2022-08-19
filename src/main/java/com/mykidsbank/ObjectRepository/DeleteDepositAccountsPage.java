75b3d54cb939299957fcbe1f4d06cdb50a876fa7
lavakumar578 <lavakumar578@gmail.com>
2022-08-19 09:24:40 +0530

    commit

--- src/main/java/com/mykidsbank/ObjectRepository/DeleteDepositAccountsPage.java
+++ src/main/java/com/mykidsbank/ObjectRepository/DeleteDepositAccountsPage.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class DeleteDepositAccountsPage {
+import com.mykidsbank.genericUtlity.BaseClass;
+import com.mykidsbank.genericUtlity.DriverUtility;
+import com.mykidsbank.genericUtlity.XlsxUtility;
+
+public class DeleteDepositAccountsPage extends BaseClass {
 
 	@FindBy(xpath="//span[contains(text(),\"select\")]")
 	private WebElement selectAllBtn;
